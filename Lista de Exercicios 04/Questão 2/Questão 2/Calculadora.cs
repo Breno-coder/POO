9b22d5af27de37c023bab0b6e38a68e1cee84786
Breno Santos <breno098526049@gmail.com>
2019-10-01 20:17:56 -0300

    atv2

---
+++ Lista de Exercicios 04/Questão 2/Questão 2/Calculadora.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_2
+{
+    abstract class Calculadora
+    {
+        public float calcular(float Numero1, float Numero2);
+    }
+
+    class somar : Calculadora
+    {
+        public override float calcular(float Numero1, float Numero2)
+        {
+            return Numero1 + Numero2;
+        }
+    }
+
+    class subtracao : Calculadora
+    {
+        public override float calcular(float Numero1, float Numero2)
+        {
+            return Numero1 - Numero2;
+        }
+    }
+
+    class multiplicacao : Calculadora
+    {
+        public override float calcular(float Numero1, float Numero2)
+        {
+            return Numero1 * Numero2;
+        }
+    }
+
+    class divisao : Calculadora
+    {
+        public override float calcular(float Numero1, float Numero2)
+        {
+            return Numero1 / Numero2;
+        }
+    }
+}
