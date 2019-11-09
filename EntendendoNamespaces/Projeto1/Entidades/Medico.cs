e6ac7100231618d8600639e4077cd3987d824bf2
Breno Santos <breno098526049@gmail.com>
2019-11-08 21:12:55 -0300

    namespace

---
+++ EntendendoNamespaces/Projeto1/Entidades/Medico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Medico
+    {
+        protected string Registro;
+        protected string Nome;
+        public DateTime DataNascimento;
+
+        protected int CalculaIdade()
+        {
+            int Idade = DateTime.Now.Year - DataNascimento.Year;
+
+            return Idade;
+        }
+    }
+}
---
+++ EntendendoNamespaces/Projeto1/Formularios/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+namespace Formularios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Medico medico = new Medico();
+        Obstreta obstreta = new Obstreta();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
